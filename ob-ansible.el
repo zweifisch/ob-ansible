cbb52a4a9ed31a88b9797c2018ab18ccad3b3432
Feng Zhou <zf.pascal@gmail.com>
2017-04-22 15:30:12 +0800

    playbook support

--- ob-ansible.el
+++ ob-ansible.el
@@ -22,7 +22,8 @@
     (hosts . :any)
     (forks . :any)
     (user . :any)
-    (become . :any))
+    (become . :any)
+    (become-user . :any))
   "ansible header arguments")
 
 (defun org-babel-execute:ansible (body params)
@@ -33,19 +34,31 @@
          (hosts (or (cdr (assoc :hosts params)) "all"))
          (forks (cdr (assoc :forks params)))
          (user (cdr (assoc :user params)))
-         (become (cdr (assoc :become params))))
-    (with-temp-file inventory-file
-      (insert inventory))
+         (become (assoc :become params))
+         (become-user (cdr (assoc :become-user params)))
+         (playbook (assoc :playbook params))
+         (args (concat " -i " inventory-file
+                     (when user (format " -u %s" user))
+                     (when become " --become")
+                     (when become-user (format " --become-user %s" become-user))
+                     (when forks (format " -f %s" forks)))))
+    (with-temp-file inventory-file (insert inventory))
     (org-babel-chomp
-     (shell-command-to-string
-      (concat "ansible"
-              (format " \"%s\"" hosts)
-              " -i " inventory-file
-              (when user (format " -u %s" user))
-              (when become (format " -b %s" become))
-              (when forks (format " -f %s" forks))
-              " -m " module
-              (format " -a \"%s\"" body))))))
+     (let ((cmd
+            (if playbook
+                (let ((playbook-file (org-babel-temp-file "ob-ansible-playbook")))
+                  (with-temp-file playbook-file (insert body))
+                  (concat "ansible-playbook" args " " playbook-file))
+              (concat "ansible"
+                      (format " \"%s\"" hosts)
+                      args
+                      " --module-name " module
+                      " --one-line"
+                      (format " --args \"%s\"" body)))))
+       (with-current-buffer (get-buffer-create "*ansible commands history*")
+         (goto-char (point-max))
+         (insert (concat cmd "\n")))
+       (shell-command-to-string cmd)))))
 
 (provide 'ob-ansible)
 ;;; ob-ansible.el ends here
